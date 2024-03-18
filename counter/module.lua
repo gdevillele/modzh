42c0e281afe8aea68eefbf7ec409f2aa65938a0c
Gaëtan de Villèle <gdevillele@proton.me>
2024-03-17 19:22:49 -0700

    wip 2

--- counter/module.lua
+++ counter/module.lua
@@ -5,8 +5,66 @@
 local module = {}
 
 local initForClient = function()
+	module.counters = {} -- TODO: REMOVE THIS
+
 	module.test = function()
 		print("Hello from counter module! 🙂 (Client)")
+	end
+
+	module.increment = function(self, userID, counterName, incrementBy)
+		-- args validation
+		if type(userID) ~= "string" then
+			error("userID must be a string", 2)
+		end
+		if type(counterName) ~= "string" then
+			error("counterName must be a string", 2)
+		end
+		if type(incrementBy) ~= "number" and type(incrementBy) ~= "integer" then
+			error("incrementBy must be a number", 2)
+		end
+
+		-- TODO: send event to server
+
+		-- --- REMOVE THIS ---
+		if self.counters[counterName] == nil then
+			self.counters[counterName] = {}
+		end
+		self.counters[counterName][userID] = (self.counters[counterName][userID] or 0) + incrementBy
+		-- -------------------
+
+		return self.counters[counterName][userID]
+	end
+
+	module.get = function(self, userID, counterName)
+		-- args validation
+		if type(userID) ~= "string" then
+			error("userID must be a string", 2)
+		end
+		if type(counterName) ~= "string" then
+			error("counterName must be a string", 2)
+		end
+		return self.counters[counterName][userID] or 0
+	end
+
+	module.runUnitTests = function()
+		print("Running UnitTests for counter module...")
+
+		local userID = "user1"
+		local counterName = "counter1"
+		local t
+
+		t = module:get(userID, counterName)
+		if t ~= 0 then
+			error("get() should return 0 when counter does not exist")
+		end
+
+		t = module:increment(userID, counterName, 1)
+		if t ~= 1 then
+			error("increment() should return 1")
+		end
+
+		print("UnitTests for counter module (client) passed! 🙂")
+		return true
 	end
 end
 
